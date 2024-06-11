7fb2ba1bfba8be65c89537f6cdf29e7ef989b1f9
Jude Mcqueen <101180204+judemcq@users.noreply.github.com>
2024-06-11 15:20:57 +0100

    Differential Phase Code

--- Differential Phase estimation.py
+++ Differential Phase estimation.py
@@ -4,7 +4,7 @@ import tensorflow_probability as tfp
 import matplotlib.pyplot as plt
 import os
 from scipy.integrate import odeint
-
+from matplotlib.animation import FuncAnimation
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -46,8 +46,8 @@ def target_log_prob_fn(c):
     return joint_log_prob(num_samples, c)
 
 # Run Hamiltonian Monte Carlo
-num_results = 100
-num_burnin_steps = 100
+num_results = 1000
+num_burnin_steps = 1000
 
 # Initial state for the chain
 initial_state = [tf.ones(num_samples, dtype=tf.float32)*np.pi]
@@ -76,7 +76,8 @@ all_y_A = []
 all_y_B = []
 all_y_A_true = []
 all_y_B_true = []
-plt.figure(figsize=(10, 8))
+
+
 for c_sample in hmc_results[0]:
     y_A, y_B = StateFcn(True)
     y_A_true, y_B_true = StateFcn(False)
@@ -84,15 +85,47 @@ for c_sample in hmc_results[0]:
     all_y_B.extend(y_B)
     all_y_A_true.extend(y_A_true)
     all_y_B_true.extend(y_B_true)
-
-plt.plot(all_y_A, all_y_B, 'o', color='green', markersize=3, alpha=0.1, label = "Noisy")
-plt.plot(all_y_A_true, all_y_B_true, color='red', markersize=3, alpha=0.1, label = "Noiseless")
+plt.figure(figsize=(10, 8))
+plt.plot(all_y_A, all_y_B, 'o', color='green', markersize=3, alpha=0.1)
+plt.plot(all_y_A_true, all_y_B_true, color='red', markersize=3, alpha=0.1)
 plt.xlabel('y_A')
 plt.ylabel('y_B')
-plt.legend()
 plt.title('Parametric Plot of y_B vs y_A for All Runs')
+plt.legend("With error", "without error")
 plt.grid(True)
 plt.show()
+
+plt.figure(figsize=(10, 8))
+plt.plot(c_sample, y_A, 'o', color='green', markersize=3, alpha=0.1)
+plt.plot(c_sample, y_B, 'o', color='green', markersize=3, alpha=0.1)
+plt.xlabel('c_samples')
+plt.ylabel('y')
+plt.title('Parametric Plot of y_B vs y_A for All Runs')
+plt.legend("With error", "without error")
+plt.grid(True)
+plt.show()
+
+def likelihoodProb(y_A, y_B, c_sample, phi_d_est):
+    equation = []
+    c_sample = c_sample.numpy()
+    for i in range(0,len(c_sample)-1):
+        partA = np.exp(-(y_A[i] - np.sin(c_sample[i] + phi_d_est))*1/(2*(np.pi/50)))*np.exp(-(y_B[i] - np.sin(c_sample[i]+2*phi_d_est)))*1/(2*np.pi/50) + np.exp(-(y_B[i] - np.sin(c_sample[i]))*1/(2*np.pi/50))
+        equation.append(partA)
+    
+    return equation
+    
+likelihoodNonInt = []
+for i in range(0,num_results-1):
+    likelihoodNonInt.append(likelihoodProb(y_A[i], y_B[i], c_sample[i], d_true))
+
+
+x = np.arange(-np.pi/2,np.pi/2,np.pi/len(likelihoodNonInt[0]))
+plt.figure(figsize=(10, 8))
+for i in range(0,num_samples-1):
+    plt.plot(x, likelihoodNonInt[i], 'o', color='green', markersize=3, label = "Noisy")
+plt.grid(True)
+plt.show()
+
 
 # Plot posterior distribution (Need to still calculate the diff phase prob)
 """plt.figure(figsize=(8, 6))
